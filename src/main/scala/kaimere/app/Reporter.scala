4c6534bdf0785863d5fb0fdf0e0d6023cc0f336f
wol4aravio <wol4aravio@yandex.ru>
2018-02-08 14:41:18 +0300

    Axis Fix + Delay

--- src/main/scala/kaimere/app/Reporter.scala
+++ src/main/scala/kaimere/app/Reporter.scala
@@ -16,7 +16,8 @@ object Reporter extends App {
     val matlabEngine = opt[String](required = true)
     val graphicsJson = opt[String](required = true)
     val width = opt[Int](default = Some(500))
-    val heigth = opt[Int](default = Some(500))
+    val height = opt[Int](default = Some(500))
+    val delay = opt[Double](default = Some(0.1))
     val saveTo = opt[String](required = true)
     verify()
   }
@@ -44,25 +45,29 @@ object Reporter extends App {
       dirs.foldLeft(Seq.empty[State]) { case (s, d) => s ++ extractStates(d)}
   }
 
-  def parseGraphicsJson(json: JsValue): Seq[((String, String, String), Seq[(Option[String], String, String)])] = {
+  def parseGraphicsJson(json: JsValue): Seq[((Int, Int, String), (Int, Int, Int, Int), Seq[(Option[String], String, String)])] = {
     json.asInstanceOf[JsArray].elements.map { jsValue_1 =>
-        val Seq(JsString(rows), JsString(cols), JsString(position), JsArray(objects)) =
+        val Seq(JsNumber(rows), JsNumber(cols), JsString(position), JsArray(objects)) =
           jsValue_1.asJsObject.getFields("rows", "cols", "position", "objects")
+      val Seq(JsArray(xLim), JsArray(yLim)) = jsValue_1.asJsObject.getFields("xLim", "yLim")
+      val Seq(xMin, xMax) = xLim.map(_.asInstanceOf[JsNumber].value.toInt)
+      val Seq(yMin, yMax) = yLim.map(_.asInstanceOf[JsNumber].value.toInt)
         val objectsToDraw = objects.map { jsValue_2 =>
             val Seq(JsString(x), JsString(y), JsString(name)) = jsValue_2.asJsObject.getFields("x", "y", "name")
           (if (x == "null") Option.empty[String] else Some(x), y, name)
         }
-      ((rows, cols, position), objectsToDraw)
+      ((rows.toInt, cols.toInt, position), (xMin, xMax, yMin, yMax), objectsToDraw)
     }
   }
 
   def drawGif(bestValues: Seq[RealVector], model: Simulink.Model,
               width: Int, height: Int,
-              whatToDraw: Seq[((String, String, String), Seq[(Option[String], String, String)])],
-              filename: String
+              whatToDraw: Seq[((Int, Int, String), (Int, Int, Int, Int), Seq[(Option[String], String, String)])],
+              delay: Double, filename: String
              ): Unit = {
     val subplot = whatToDraw.map(_._1)
-    val allAxes = whatToDraw.map(_._2)
+    val limits = whatToDraw.map(_._2)
+    val allAxes = whatToDraw.map(_._3)
     val numberOfPlots = subplot.size
     Matlab.eval(s"slideShow = figure('Units', 'pixels', 'Position', [0, 0, $width, $height], 'Visible', 'off');")
     Matlab.eval("axis tight manual")
@@ -70,7 +75,7 @@ object Reporter extends App {
     bestValues.zipWithIndex.foreach { case (v, slideId) =>
       model(v)
       Range(0, numberOfPlots).foreach { plotId =>
-        val (w, h, n) = subplot(plotId)
+        val (w, h, p) = subplot(plotId)
         val axes = allAxes(plotId).map { case (x, y, name) =>
           if (x.isEmpty) (s"$y.Time", s"$y.Data", s"'$name'")
           else (s"${x.get}.Data", s"$y.Data", s"'name'")
@@ -78,15 +83,19 @@ object Reporter extends App {
         val xAxis = axes.map(_._1)
         val yAxis = axes.map(_._2)
         val names = axes.map(_._3)
-        Matlab.eval(s"subplot($w, $h, $n)")
-        Matlab.eval(s"plot([${xAxis.mkString(", ")}], [${yAxis.mkString(", ")}])")
+        val (xMin, xMax, yMin, yMax) = limits(plotId)
+        Matlab.eval(s"subplot($w, $h, $p)")
+        Matlab.eval(s"plot([${xAxis.mkString(", ")}], [${yAxis.mkString(", ")}], 'LineWidth', 2)")
+        Matlab.eval(s"xlim([$xMin, $xMax])")
+        Matlab.eval(s"ylim([$yMin, $yMax])")
         Matlab.eval(s"legend(${names.mkString(", ")}, 'Location', 'southoutside', 'Orientation', 'horizontal')")
+        Matlab.eval(s"title('Iteration: ${slideId + 1}')")
       }
       Matlab.eval("drawnow")
       Matlab.eval("frame = getframe(slideShow);")
       Matlab.eval("im = frame2im(frame);")
       Matlab.eval("[imind, cm] = rgb2ind(im, 256);")
-      Matlab.eval(s"if ($slideId == 0) imwrite(imind, cm, filename, 'gif', 'Loopcount', inf); else imwrite(imind, cm, filename, 'gif', 'WriteMode','append'); end")
+      Matlab.eval(s"if ($slideId == 0) imwrite(imind, cm, filename, 'gif', 'Loopcount', inf, 'DelayTime', $delay); else imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append', 'DelayTime', $delay); end")
     }
   }
 
@@ -111,7 +120,7 @@ object Reporter extends App {
 
     println("Making graphics")
     val graphicsJson = Source.fromFile(conf.graphicsJson()).mkString.parseJson
-    drawGif(bestValues, model, conf.width(), conf.heigth(), parseGraphicsJson(graphicsJson), conf.saveTo())
+    drawGif(bestValues, model, conf.width(), conf.height(), parseGraphicsJson(graphicsJson), conf.delay(), conf.saveTo())
 
     println("Terminating Matlab Engine")
     terminate(conf, simulinkModelSlx)
