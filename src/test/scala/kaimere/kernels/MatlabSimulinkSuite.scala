dd190664374f5d0123ee5968cfa41438ebd6ee9b
wol4aravio <wol4aravio@yandex.ru>
2018-02-14 09:07:36 +0300

    Resource Rename

--- src/test/scala/kaimere/kernels/MatlabSimulinkSuite.scala
+++ src/test/scala/kaimere/kernels/MatlabSimulinkSuite.scala
@@ -16,8 +16,8 @@ class MatlabSimulinkSuite extends FunSuite with BeforeAndAfterAll {
   }
 
   test("Orientation Test") {
-    val modelLocation = "./resources/test/SpacecraftOrientation.slx"
-    val jsonLocation = "./resources/test/SpacecraftOrientation.json"
+    val modelLocation = "./resources/MatlabSimulinkTest/SpacecraftOrientation.slx"
+    val jsonLocation = "./resources/MatlabSimulinkTest/SpacecraftOrientation.json"
     val model = Matlab.loadSimulinkModel(
       model = modelLocation,
       jsonConfig = jsonLocation)
